751968703a2d9ee9c8a5da3667c78bd82c0cced8
Daniel Santos <dfsantosbu@unal.edu.co>
2017-08-31 20:28:50 -0500

    Modularize new types of data created.

---
+++ cpp/types.hpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+/*
+ *  New types
+ */
+typedef vector<Point> vp;
+typedef vector<vp> vvp;
+typedef vector<Vec4i> v4;
+typedef vector<KeyPoint> vkp;
+typedef vector<DMatch> vdm;
+typedef vector<Point2f> vp2;
