c878c210e0c23733cd04a2d87fd0ab40beed3deb
Daniel Santos <dfsantosbu@unal.edu.co>
2017-08-31 20:28:08 -0500

    Modularize the process of getting movements.

---
+++ cpp/movement.hpp
@@ -0,0 +1,26 @@
+using namespace std;
+
+int THRESH_VALUE = 50;
+
+pair < vvp, v4>  getMovements(Mat frame1, Mat frame2) {
+	Mat grayImg1, grayImg2;
+	Mat temp, diffImg, threshImg, blurImg;
+
+	toGrayI(frame1, grayImg1);
+	toGrayI(frame2, grayImg2);
+
+	diffI(grayImg2, grayImg1, diffImg);
+	threshI(diffImg, threshImg, THRESH_VALUE);
+	blurI(threshImg, blurImg);
+
+	vvp contours;
+	v4 hierarchy;
+
+	threshImg.copyTo(temp);
+	findContoursI(temp, contours, hierarchy);
+
+	if (contours.size() > 0) {
+		printf("The number of contours detected is: %d\n", contours.size());
+	}
+	return mp(contours, hierarchy);
+}
